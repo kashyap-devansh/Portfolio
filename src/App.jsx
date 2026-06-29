e45b2901d842e7606f1c912db87a6165bd7d01bf
Devansh Kashyap <devansh14007@gmail.com>
2026-06-29 06:34:24 +0530

    Created WhatICanDo and Integrated in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import Intro from "./components/Intro/Intro.jsx";
+import WhatICanDO from "./components/WhatICanDo/WhatICanDo.jsx";
 import Work from "./components/Work/Work.jsx";
 import Contact from "./components/Contact/contact-Hero.jsx";
 import ContactCTA from "./components/Contact/ContactCTA.jsx";
@@ -13,6 +14,7 @@ const App = () => {
       <Navbar />
       <Intro />
       <Work />
+      <WhatICanDO />
       <Contact />
       <ContactCTA />
       <Footer />
---
+++ src/components/WhatICanDo/WhatICanDo.jsx
@@ -0,0 +1,24 @@
+import "./WhatICanDo.css"
+
+const WhatICanDo = () => {
+  return (
+    <div className="whatICanDo">
+      <p>WHAT I CAN Do</p>
+
+      <div className="expertises-wrapper">
+
+        <div className="experties">
+          <h1>Programming Languages</h1>
+          <h1>Backend Development</h1>
+          <h1>System programming</h1>
+          <h1>Compiler Design</h1>
+          <h1>Linux & Open Source</h1>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default WhatICanDo
