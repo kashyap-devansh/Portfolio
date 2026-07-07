27722e5199861de1697c9d5b82295342cedb9fd9
Devansh Kashyap <devansh14007@gmail.com>
2026-07-07 14:25:16 +0530

    Added animation in Name in AboutMe

--- src/pages/AboutMe/Name/Name.jsx
+++ src/pages/AboutMe/Name/Name.jsx
@@ -1,8 +1,24 @@
+import { motion } from "framer-motion";
 import "./Name.css";
 const Name = () => {
   return (
     <div className="name">
-      <h1>DEVΛNSH KΛSHYΛP</h1>
+      <motion.h1
+        initial={{
+          opacity: 0, y: 150
+        }}
+        whileInView={{
+          opacity: 1, y: 0
+        }}
+        viewport={{
+          once: true
+        }}
+        transition={{
+          duration: 0.8, ease: "easeOut"
+        }}
+      >
+        DEVΛNSH KΛSHYΛP
+      </motion.h1>
 
     </div>
   )
