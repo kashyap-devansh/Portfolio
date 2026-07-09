352aa506a6ab5dd4f7e01a06fe6e52519d639185
Devansh Kashyap <devansh14007@gmail.com>
2026-07-09 16:49:42 +0530

    Added motion in BackStory.jsx

--- src/pages/AboutMe/BackStory/BackStory.jsx
+++ src/pages/AboutMe/BackStory/BackStory.jsx
@@ -1,4 +1,31 @@
+import { motion } from "framer-motion";
 import "./BackStory.css";
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      delayChildren: 0.35,
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const info = {
+  hidden: {
+    opacity: 0,
+    y: 300,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      ease: [0.25, 1, 0.5, 1],
+    },
+  },
+};
+
 
 const BackStory = () => {
   return (
@@ -6,33 +33,54 @@ const BackStory = () => {
       <div className="backstory-content">
         <p>backstory</p>
 
-        <div className="info">
-          <div className="info-item">
+        <motion.div
+          className="info"
+          variants={container}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.1 }}
+        >
+          <motion.div
+            className="info-item"
+            variants={info}
+          >
             <h6 className="info-label">race :</h6>
             <h6 className="info-value">human</h6>
-          </div>
+          </motion.div>
 
-          <div className="info-item">
+          <motion.div
+            className="info-item"
+            variants={info}
+          >
             <h6 className="info-label">age :</h6>
             <h6 className="info-value">19</h6>
-          </div>
+          </motion.div>
 
-          <div className="info-item">
+          <motion.div
+            className="info-item"
+            variants={info}
+          >
             <h6 className="info-label">languages :</h6>
             <h6 className="info-value">hindi/english</h6>
-          </div>
+          </motion.div>
 
-          <div className="info-item">
+          <motion.div
+            className="info-item"
+            variants={info}
+          >
             <h6 className="info-label">finanical :</h6>
             <h6 className="info-value">broke</h6>
-          </div>
+          </motion.div>
 
-          <div className="info-item">
+          <motion.div
+            className="info-item"
+            variants={info}
+          >
             <h6 className="info-label">height</h6>
             <h6 className="info-value">classified</h6>
-          </div>
+          </motion.div>
 
-        </div>
+        </motion.div>
       </div>
     </div>
   )
